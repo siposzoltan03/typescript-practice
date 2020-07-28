dc78b689eaff2987a8a3de68e0868ef9b6591924
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-07-28 14:40:38 +0200

    Add cards to boards

--- proman-typescript/src/components/Board/Board.tsx
+++ proman-typescript/src/components/Board/Board.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import './Board.css';
-import BoardColumn from '../BoardColumn/BoardColumn';
+import React, { useState } from 'react'
+import './Board.css'
+import BoardColumn from '../BoardColumn/BoardColumn'
+import Card from '../Card/Card'
 
-interface Props{
-    children: React.ReactNode,
+interface Props {
+    children: React.ReactNode
 }
 
 export default function Board<Props>() {
+    const [cards, setCards] = useState<string[]>([
+        'Card 1',
+        'Card 2',
+        'Card 3',
+        'Card 4',
+    ])
+    const [columns, setColumns] = useState<string[]>([
+        'New',
+        'In progress',
+        'Testing',
+        'Done',
+    ])
+
     return (
         <section className="board">
-            <div className="board-header"><span className="board-title">Board 1</span>
+            <div className="board-header">
+                <span className="board-title">Board 1</span>
                 <button className="board-add">Add Card</button>
-                <button className="board-toggle"><i className="fas fa-chevron-down"></i></button>
+                <button className="board-toggle">
+                    <i className="fas fa-chevron-down"></i>
+                </button>
             </div>
             <div className="board-columns">
-                <BoardColumn title="New" />
-                <BoardColumn title="In Progress" />
-                <BoardColumn title="Testing" />
-                <BoardColumn title="Done" />
+                {columns.map((column, index) => (
+                    <BoardColumn
+                        key={index}
+                        title={column}
+                        cards={cards.map((card, index) => (
+                            <Card key={index} text={card} />
+                        ))}
+                    />
+                ))}
             </div>
-            </section>
+        </section>
     )
 }
--- proman-typescript/src/components/BoardColumn/BoardColumn.tsx
+++ proman-typescript/src/components/BoardColumn/BoardColumn.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import './BoardColumn.css'
 
 type Props = {
-    children?: React.ReactNode,
+    cards?: React.ReactNode[],
     title: string
 }
 
-const BoardColumn = ({children, title}: Props): JSX.Element => {
+const BoardColumn = ({cards, title}: Props): JSX.Element => {
     return (
                 <div className="board-column">
                 <div className="board-column-title">{title}</div>
                     <div className="board-column-content">
-                        {children}
+                        {cards}
                     </div>
                 </div>
     
