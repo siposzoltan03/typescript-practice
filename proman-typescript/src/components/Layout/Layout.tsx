3556e06d42ad467c783d75aa1d94ad6e8928947e
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-07-31 13:19:38 +0200

    Add handleAddboard func to Layout component

--- proman-typescript/src/components/Layout/Layout.tsx
+++ proman-typescript/src/components/Layout/Layout.tsx
@@ -1,8 +1,9 @@
-import React, { Children, useContext, ContextType } from "react";
+import React, { Children, useContext, ContextType, useState } from "react";
 import "./Layout.css";
 import Board from "../Board/Board";
 import { DataContext } from "../../data/dataContext";
-import { boards, columns, cards } from "../../data/dataObject";
+import { boards, columns, cards, board } from "../../data/dataObject";
+import { utils } from "../../utils/utils";
 
 interface Props {
 	children: React.ReactNode;
@@ -13,14 +14,28 @@ export default function Layout<Props>(): JSX.Element {
 	// const boards = dataContext?.boards;
 	// const lists = dataContext?.lists;
 
+	const [boardsState, setBoardsState] = useState<board[]>(boards);
+
+	const handleAddBoard = () => {
+		const newBoardsState = [...boardsState];
+		const newBoard: board = {
+			id: utils.generateId(boardsState),
+			title: `Board ${utils.generateId(boardsState)}`,
+			columnIds: [1, 2, 3, 4],
+		};
+		newBoardsState.push(newBoard);
+		setBoardsState(newBoardsState);
+	};
 	return (
 		<div className="container">
 			<div className="title-line">
 				<div className="title">Proman</div>
-				<button className="add-board">Add Board</button>
+				<button className="add-board" onClick={handleAddBoard}>
+					Add Board
+				</button>
 			</div>
 			<div className="board-container">
-				{boards?.map((board, index: number) => {
+				{boardsState?.map((board, index: number) => {
 					return (
 						<Board
 							key={index}
